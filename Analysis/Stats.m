748cde0aa8945de7feb414b2be992cc337171c46
ThreeGoldZ <zhuxin@bjtu.edu.cn>
2022-08-04 10:48:12 -0700

    plots

--- Analysis/Stats.m
+++ Analysis/Stats.m
@@ -1,3 +1,6 @@
+clear all
+close all
+
 filename ='Study/threshold.csv';
 M = csvread(filename,1,0);
 j=6
@@ -17,4 +20,34 @@ for i =1:389
 end
 
 writematrix(Data,'Study/threshold_part.csv')
+
+G = findgroups(Data(:,5))
+
+Y = splitapply(@mean,Data,G)
+
+plot((1:20),Y(:,3),'.')
+hold on 
+plot((1:20),Y(:,4),'.')
+hold off 
+%boxplot of perception 
+boxplot(Data(:,3),Data(:,2))
+
+%boxplot of tolerance 
+%boxplot(Data(:,4),Data(:,5))
+ylim([0 40])
+
+[p,t,stats1] = anova1(Data(:,3),Data(:,2),"subjects")
+[c,m,h] = multcompare(stats1)
+
+%Calculate the mean of perception threshold for each person 
+for i = 1:116
+    for k=1:20
+        %if Data(i,5)=k
+
+        
+    end
+end
+
+%Calculate the mean of tolerance for each person
+
        
