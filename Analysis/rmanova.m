facf6803acbdd43b6517d369deff99f0220b64ee
ThreeGoldZ <zhuxin@bjtu.edu.cn>
2022-08-12 17:22:12 -0700

    update data process

---
+++ Analysis/rmanova.m
@@ -0,0 +1,76 @@
+
+filename ='Study/threshold_23.csv';
+data = csvread(filename,1,0);
+
+re= find(data(:,1)==1)
+loc1_min=[]
+loc2_min=[]
+loc3_min=[]
+loc4_min=[]
+loc5_min=[]
+loc6_min=[]
+loc1_max=[]
+loc2_max=[]
+loc3_max=[]
+loc4_max=[]
+loc5_max=[]
+loc6_max=[]
+p_number =[]
+i =1
+while i<121
+    p_number = [p_number data(i,1)]
+    i = i+6
+end
+%get update from pranavi about subject gender
+Gender = ['F' 'F' 'F' 'F' 'F' 'F' 'F' 'F' 'M' 'M' 'M' 'M' 'M' 'M' 'M' 'M''F''F''F''F'];
+for i=1:120
+    if  data(i,4)==1
+        loc1_min=[loc1_min data(i,7)]
+    end
+    if  data(i,4)==2
+        loc2_min=[loc2_min data(i,7)]
+    end
+    if  data(i,4)==3
+        loc3_min=[loc3_min data(i,7)]
+    end
+    if  data(i,4)==4
+        loc4_min=[loc4_min data(i,7)]
+    end
+    if  data(i,4)==5
+        loc5_min=[loc5_min data(i,7)]
+    end
+    if  data(i,4)==6
+        loc6_min=[loc6_min data(i,7)]
+    end
+end
+
+for i=1:120
+    if  data(i,4)==1
+        loc1_max=[loc1_max data(i,8)]
+    end
+    if  data(i,4)==2
+        loc2_max=[loc2_max data(i,8)]
+    end
+    if  data(i,4)==3
+        loc3_max=[loc3_max data(i,8)]
+    end
+    if  data(i,4)==4
+        loc4_max=[loc4_max data(i,8)]
+    end
+    if  data(i,4)==5
+        loc5_max=[loc5_max data(i,8)]
+    end
+    if  data(i,4)==6
+        loc6_max=[loc6_max data(i,8)]
+    end
+end
+
+Location=[1 2 3 4 5 6]';
+
+t_min = table(p_number',loc1_min',loc2_min',loc3_min',loc4_min',loc5_min',loc6_min','VariableNames',{'SubjectNumber','l1','l2','l3','l4','l5','l6'});
+rm = fitrm(t_min,'l1-l6 ~ SubjectNumber','WithinDesign',Location,'WithinModel','orthogonalcontrasts')
+
+T = multcompare(rm,'Time')
+ranovatbl = ranova(rm)
+%[ranovatbl,A,C,D] = ranova(rm,'WithinModel',Location)
+%multcompare(ranovatbl)
